c2834f1acd4c6b097cd2de36d3dfbac4e0a7eb1b
Pierluigi <Pierluigi@10.7.30.189>
2017-05-30 22:52:25 +0200

    prima parte fatta!

--- src/it/polito/tdp/libretto/LibrettoController.java
+++ src/it/polito/tdp/libretto/LibrettoController.java
@@ -1,5 +1,97 @@
 package it.polito.tdp.libretto;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.libretto.model.Esame;
+import it.polito.tdp.libretto.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class LibrettoController {
-	
+	Model model;
+
+    public void setModel(Model model) {
+		this.model = model;
+	}
+
+	@FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtCodice;
+
+    @FXML
+    private TextField txtTitolo;
+
+    @FXML
+    private TextField txtDocente;
+
+    @FXML
+    private Button btnCerca;
+
+    @FXML
+    private Button btnInserisci;
+
+    @FXML
+    private TextArea txtinfo;
+
+    @FXML
+    void doCerca(ActionEvent event) {
+    	String codice = txtCodice.getText();
+    	if(codice.length()<5){
+    		return;
+    	}
+    	Esame e = model.trovaEsame(codice);
+    	if(e==null){
+    		txtinfo.appendText("Codice non trovato\n");
+    	}
+    	else {
+    		txtinfo.appendText("Codice trovato\n");
+    		txtCodice.setText(e.getCodice());
+    		txtTitolo.setText(e.getTitolo());
+    		txtDocente.setText(e.getDocente());
+    	}
+
+    }
+
+    @FXML
+    void doInserisci(ActionEvent event) {
+    	String codice = txtCodice.getText();
+    	String titolo = txtTitolo.getText();
+    	String docente = txtDocente.getText();
+    	
+    	if(codice.length()<5 || titolo.length()==0 || docente.length()==0 ){
+    		txtinfo.appendText("Inserire i dati correttamente\n");
+    		return;
+    	}
+    	Esame e = new Esame(codice, titolo, docente);
+    	boolean result = model.addEsame(e);
+    	
+    	if(result){
+    		txtinfo.appendText("Esame aggiunto correttamente\n");
+    	}
+    	else{
+    		txtinfo.appendText("Esame non aggiunto correttamente(Codice duplicato)\n");
+    	}
+    	
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtCodice != null : "fx:id=\"txtCodice\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert txtTitolo != null : "fx:id=\"txtTitolo\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert txtDocente != null : "fx:id=\"txtDocente\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert btnCerca != null : "fx:id=\"btnCerca\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert txtinfo != null : "fx:id=\"txtinfo\" was not injected: check your FXML file 'Libretto.fxml'.";
+
+    }
 }
--- src/it/polito/tdp/libretto/model/Model.java
+++ src/it/polito/tdp/libretto/model/Model.java
@@ -1,5 +1,29 @@
 package it.polito.tdp.libretto.model;
 
-public class Model {
+import java.util.*;
 
+public class Model {
+	private List<Esame> esami;
+		
+		public Model(){
+			this.esami = new ArrayList<Esame>();
+			
+		}
+		
+		public boolean addEsame(Esame e){		
+			if(!esami.contains(e)){
+			esami.add(e);
+			return true;
+			}
+			else 
+				return false;
+		}
+		
+		public Esame trovaEsame(String codice){
+			int pos = this.esami.indexOf(new Esame(codice, null, null));
+			if(pos==-1){
+				return null;
+			}
+			else return esami.get(pos);
+		}
 }
